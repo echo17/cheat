6c849c5f3f526a416220cfd9d0001baf07d6e90a
Aaron Lesley <echoside@gmail.com>
2015-04-09 17:55:02 -0700

    Remove test button

--- cheat.py
+++ cheat.py
@@ -5,9 +5,6 @@ from subprocess import call
 
 top = Tkinter.Tk()
 top.title("Cheat")
-
-def helloCallBack():
-   tkMessageBox.showinfo( "Hello Python", "Hello World")
    
 def ping():
 	E1F = E1.get()
@@ -32,14 +29,12 @@ complabel.pack()
 E1 = Entry(top, bd = 5)
 E1.pack()	
 
-B = Tkinter.Button(top, text ="Hello", width=20, command = helloCallBack)
 B2 = Tkinter.Button(top, text ="Ping", width=20, command = ping)
 B3 = Tkinter.Button(top, text ="WDS", width=20, command = helloCallBack)
 B4 = Tkinter.Button(top, text ="Tasklist", width=20, command = tasklist)
 B5 = Tkinter.Button(top, text ="SN", width=20, command = sn)
 B6 = Tkinter.Button(top, text ="Reset Spool", width=20, command = helloCallBack)
 
-B.pack()
 B2.pack()
 B3.pack()
 B4.pack()
